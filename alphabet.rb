327284dabc8a077eb970c4f8826cd7b9340b337f
Christian Blunden <christian.blunden@gmail.com>
2012-01-23 22:27:17 +0000

    alphabet soup

---
+++ alphabet.rb
@@ -0,0 +1,22 @@
+HACKERCUP = "HACKERCUP".each_char.map{|c| c}
+def count_for(characters)
+  counts = Hash.new(0)
+  characters.each{|c| 
+    counts[c] += 1 if HACKERCUP.include?(c) 
+  }
+  return 0 if counts.length < 8
+  counts["C"] = (counts["C"]/2).floor
+  return counts.values.min
+end
+
+def main
+  output = File.open('alphabet_output.txt', 'w')
+  lines = File.readlines('alphabet_example.txt')
+  count = lines[0].to_i
+  for i in 1..count do
+    characters = lines[i].each_char
+    output.puts("Case ##{i}: #{count_for(characters)}")
+  end
+end
+
+main()
